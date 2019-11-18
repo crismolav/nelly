80b1551997255b572dd55b07b4858841be6114de
crismolav <cmoraleso@utexas.edu>
2019-11-18 18:42:45 +0100

    order class

--- nelly.py
+++ nelly.py
@@ -9,6 +9,29 @@ import pyttsx3
 from spacy import displacy
 from pdb import set_trace
 
+class Order:
+    def __init__(self):
+        self.bread_type = None
+        self.protein = None
+        self.sauce_list = []
+        self.vegetable_list = []
+    def add_bread_type(self, bread_type):
+        self.bread_type = bread_type
+    def available_bread_types(self):
+        return ['vegan', 'whole-wheat', 'regular']
+    def add_protein_type(self, protein_type):
+        self.protein = protein_type
+    def available_protein_types(self):
+        return ['tofu', 'chicken', 'beef', 'pork']
+    def add_vegetable(self, vegetable):
+        self.vegetable_list.append(vegetable)
+    def available_vegetables(self):
+        return ['lettuce', 'tomato', 'cucumber', 'olive', 'onion']
+    def add_sauce(self, sauce):
+        self.sauce_list.append(sauce)
+    def available_sauces(self):
+        return ['barbecue', 'ketchup', 'mustard', 'nelly_sauce']
+
 def determine_semantic_frame_from_tree_root(parse_tree_root):
     if parse_tree_root in ['sandwich', 'have', 'like', 'want','give', 'need']:
         return 'request'
@@ -32,19 +55,24 @@ if __name__=="__main__":
     engine.runAndWait()
 
     # en_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
-
+    new_order = Order()
+    new_order.add_bread_type("regular")
+    new_order.add_vegetable("tomato")
+    new_order.add_vegetable("lettuce")
+    
     nlp = spacy.load("en_core_web_sm")
     doc = nlp("Can I have a sandwich with tofu and tomato")
     doc = nlp("I would like a sandwich")
+    doc = nlp("is this gluten free?")
     # doc = nlp("A sandwich with bacon and lettuce")
     tree_root_str = get_parse_tree_root(parse_tree=doc)
     semantic_frame = determine_semantic_frame_from_tree_root(parse_tree_root=tree_root_str)
 
     print("tree_root_str: %s"%tree_root_str)
     print("semantic_frame: %s"%semantic_frame)
-    # for token in doc:
-    #     print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
-    #           token.shape_, token.is_alpha, token.is_stop)
+    for token in doc:
+        print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+              token.shape_, token.is_alpha, token.is_stop)
     # displacy.serve(doc, style="dep")
     # Record Audio
     # r = sr.Recognizer()
