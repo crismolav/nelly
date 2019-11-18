cb95db9ac08228196170310a7078f74f3c9257a5
crismolav <cmoraleso@utexas.edu>
2019-11-18 19:23:31 +0100

    modify order function

--- nelly.py
+++ nelly.py
@@ -10,48 +10,62 @@ from spacy import displacy
 from pdb import set_trace
 from order import Order
 
-
-
-def determine_semantic_frame_from_tree_root(parse_tree):
-    tree_root = get_parse_tree_root(parse_tree)
+def determine_semantic_frame_from_tree_root(parsed_tree):
+    tree_root = get_parse_tree_root(parsed_tree)
     print("tree_root_str: %s" % tree_root)
     if tree_root in ['sandwich', 'have', 'like', 'want','give', 'need']:
         return 'request'
     else:
         return False
 
-def get_parse_tree_root(parse_tree):
-    for token in parse_tree:
+def get_parse_tree_root(parsed_tree):
+    for token in parsed_tree:
         if token.dep_ == 'ROOT':
             return token.lemma_
 
+def modify_order(order, parsed_tree):
+    semantic_frame = determine_semantic_frame_from_tree_root(parsed_tree)
+    print("semantic_frame: %s" % semantic_frame)
+    if semantic_frame == 'request':
+        modify_order_with_request(order=order, parsed_tree=parsed_tree)
+    else:
+        pass
+
+def modify_order_with_request(order, parsed_tree):
+    for token in parsed_tree:
+        all_ingredients = order.get_all_avaible_ingredient()
+        if token.lemma_ in order.available_protein_types():
+            order.add_protein_type(protein_type=token.lemma_)
+        elif token.lemma_ in order.available_vegetables():
+            order.add_vegetable(vegetable=token.lemma_)
+        elif token.lemma_ in order.available_sauces():
+            order.add_sauce(sauce=token.lemma_)
 
 if __name__=="__main__":
-    engine = pyttsx3.init()
-    #engine.setProperty('voice', en_voice_id)
-
-    # Set properties _before_ you add things to say
-    engine.setProperty('rate', 100)    # Speed percent (can go over 100)
-    engine.setProperty('volume', 0.9)  # Volume 0-1
-    engine.say("Thank you")
-    engine.runAndWait()
+    # engine = pyttsx3.init()
+    #     # #engine.setProperty('voice', en_voice_id)
+    #     #
+    #     # # Set properties _before_ you add things to say
+    #     # engine.setProperty('rate', 100)    # Speed percent (can go over 100)
+    #     # engine.setProperty('volume', 0.9)  # Volume 0-1
+    #     # engine.say("Thank you")
+    #     # engine.runAndWait()
 
     # en_voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
     new_order = Order()
-    new_order.add_bread_type("regular")
-    new_order.add_vegetable("tomato")
-    new_order.add_vegetable("lettuce")
-    print(new_order.vegetable_list)
+    # new_order.add_bread_type("regular")
+    # new_order.add_vegetable("tomato")
+    # new_order.add_vegetable("lettuce")
+    # print(new_order.vegetable_list)
 
     nlp = spacy.load("en_core_web_sm")
-    doc = nlp("Can I have a sandwich with tofu and tomato")
-    doc = nlp("I would like a sandwich")
-    doc = nlp("is this gluten free?")
+    # doc = nlp("I would like a sandwich")
+    # doc = nlp("is this gluten free?")
     # doc = nlp("A sandwich with bacon and lettuce")
-    semantic_frame = determine_semantic_frame_from_tree_root(parse_tree=doc)
+    doc = nlp("Can I have a sandwich with tofu and tomatoes")
 
+    modify_order(order=new_order, parsed_tree=doc)
 
-    print("semantic_frame: %s"%semantic_frame)
     for token in doc:
         print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
               token.shape_, token.is_alpha, token.is_stop)
