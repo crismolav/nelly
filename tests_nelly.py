740a6cd322c37defe9241e976debdf7b99a023a8
crismolav <cmoraleso@utexas.edu>
2019-12-03 18:17:20 +0100

    tests added

---
+++ tests_nelly.py
@@ -0,0 +1,28 @@
+import unittest
+import spacy
+import nelly
+
+class NellyTests(unittest.TestCase):
+    def test_determine_semantic_frame_from_parsed_tree__request_special_need__have(self):
+        nlp = spacy.load("en_core_web_sm")
+        parsed_tree = nlp("I have gluten allergy")
+
+        result = nelly.determine_semantic_frame_from_parsed_tree(
+            parsed_tree=parsed_tree)
+        expected = 'request_special_need'
+
+        self.assertEqual(expected, result)
+
+    def test_determine_semantic_frame_from_parsed_tree__request_special_need__be(self):
+        nlp = spacy.load("en_core_web_sm")
+        parsed_tree = nlp("I am celiac")
+
+        result = nelly.determine_semantic_frame_from_parsed_tree(
+            parsed_tree=parsed_tree)
+        expected = 'request_special_need'
+
+        self.assertEqual(expected, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
